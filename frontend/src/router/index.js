806e96b975dea473c826337600bf4bc0ee49ea1a
Nuelsax <51851349+Nuelsax@users.noreply.github.com>
2022-03-04 19:03:21 +0100

    Add files via upload
    frontend comit

---
+++ frontend/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'Register', 
+    component: RegisterView
+  },
+  {
+    path: '/',
+    name: 'Register', 
+    component: RegisterView
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
